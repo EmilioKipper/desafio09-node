925569adee94f87098348e8a898f1bab71ac8c74
Emílio Gularte Kipper <emilio_gularte@outlook.com>
2020-05-19 20:10:11 -0300

    Challenge finished

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -6,20 +6,38 @@ import {
   OneToOne,
   JoinColumn,
   OneToMany,
+  Column,
 } from 'typeorm';
+
+import { Exclude } from 'class-transformer';
 
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
 
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
+  customer_id: string;
+
+  @OneToOne(() => Customer, { eager: true })
+  @JoinColumn({ name: 'customer_id' })
   customer: Customer;
 
+  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
+    cascade: true,
+    eager: true,
+  })
   order_products: OrdersProducts[];
 
+  @CreateDateColumn()
+  @Exclude()
   created_at: Date;
 
+  @UpdateDateColumn()
+  @Exclude()
   updated_at: Date;
 }
 
